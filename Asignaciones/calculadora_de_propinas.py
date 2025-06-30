b26922444253db79db0a37b97328fc67d57da0f1
Z7NR <161151111+Z7NR@users.noreply.github.com>
2025-06-30 11:52:18 -0400

    Add abs
    se agrego abs para evitar dar resultados negativo y se simplifico el uso de decoradores

--- Asignaciones/calculadora_de_propinas.py
+++ Asignaciones/calculadora_de_propinas.py
@@ -13,30 +13,30 @@ Tareas:
 """
 
 
-print("/////////////////////////////////////////////////////////\n")
+print("/" * 57, "\n")
 total = int(input("Introduzca monto a pagar:"))
 
-print("\n/////////////////////////////////////////////////////////\n")
+print("\n","/" * 57, "\n")
 valo = int(input("¿Cual fue la valoracion sobre el servicio?: \n\n 1-Excelente \n 2-Bueno \n 3-Regular \n\n:"))
 
-print("\n/////////////////////////////////////////////////////////\n")
+print("\n","/" * 57, "\n")
 div = int(input("¿Quiere dividir la cuenta? \n\n 0-No \n 1-Si \n\n:"))
 boole = bool(div)
 
 if div == True:
-    print("\n/////////////////////////////////////////////////////////\n")
+    print("\n","/" * 57, "\n")
     pers = int(input("Introduce la cantidad de personas:"))
 
 if valo == 1:
     
     propina = total * 0.15
 
-    print("\n/////////////////////////////////////////////////////////")
-    print("\n El monto asciende a:", propina + total)
-    print("\n La propina a pagar es de:", propina)
+    print("\n","/" * 57, "\n")
+    print("\n El monto asciende a:", abs(propina + total))
+    print("\n La propina a pagar es de:", abs(propina))
     while div == True:
     
-        print("\n Monto por persona:", total / pers )
+        print("\n Monto por persona:", abs(total / pers ))
         break
     
 
@@ -44,26 +44,26 @@ elif valo == 2:
 
     propina = total * 0.12
 
-    print("\n/////////////////////////////////////////////////////////")
-    print("\n El monto asciende a:", propina + total)
-    print("\n La propina a pagar es de:", propina)
+    print("\n","/" * 57, "\n")
+    print("\n El monto asciende a:", abs(propina + total))
+    print("\n La propina a pagar es de:", abs(propina))
    
     while div == True:
     
-        print("\n Monto por persona:", total / pers )
+        print("\n Monto por persona:", abs(total / pers ))
         break
 
 elif valo == 3:
 
     propina = total * 0.10
 
-    print("\n/////////////////////////////////////////////////////////")
-    print("\n El monto asciende a:", propina + total)
-    print("\n La propina a pagar es de:", propina)
+    print("\n","/" * 57, "\n")
+    print("\n El monto asciende a:", abs(propina + total))
+    print("\n La propina a pagar es de:", abs(propina))
     
     while div == True:
     
-        print("\n Monto por persona:", total / pers )
+        print("\n Monto por persona:", abs(total / pers ))
         break
 else:
 
