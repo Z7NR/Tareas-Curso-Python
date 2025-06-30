2bcea36ffec706a00acf522fd496e010cf44fddb
Z7NR <161151111+Z7NR@users.noreply.github.com>
2025-06-29 20:08:10 -0400

    Asignacion extra #1
    Calculadora de propinas. Funcional. Sopesando la posibilidad de incluir bucles que impidan colocar numeros o datos no solicitados

---
+++ Asignaciones/calculadora_de_propinas.py
@@ -0,0 +1,71 @@
+"""
+Ejercicio 1: Calculadora de Propinas
+Contexto: Acabas de cenar en un restaurante y quieres calcular la propina  
+Tareas: 
+1. Pedir al usuario el total de la cuenta  
+2. Preguntar si el servicio fue excelente (15%), bueno (12%) o regular (10%)  
+3. Calcular la propina según la calidad del servicio  
+4. Preguntar si dividir la cuenta entre N personas  
+5. Mostrar:  
+   - Total con propina  
+   - Propina individual  
+   - Total por persona  
+"""
+
+
+print("/////////////////////////////////////////////////////////\n")
+total = int(input("Introduzca monto a pagar:"))
+
+print("\n/////////////////////////////////////////////////////////\n")
+valo = int(input("¿Cual fue la valoracion sobre el servicio?: \n\n 1-Excelente \n 2-Bueno \n 3-Regular \n\n:"))
+
+print("\n/////////////////////////////////////////////////////////\n")
+div = int(input("¿Quiere dividir la cuenta? \n\n 0-No \n 1-Si \n\n:"))
+boole = bool(div)
+
+if div == True:
+    print("\n/////////////////////////////////////////////////////////\n")
+    pers = int(input("Introduce la cantidad de personas:"))
+
+if valo == 1:
+    
+    propina = total * 0.15
+
+    print("\n/////////////////////////////////////////////////////////")
+    print("\n El monto asciende a:", propina + total)
+    print("\n La propina a pagar es de:", propina)
+    while div == True:
+    
+        print("\n Monto por persona:", total / pers )
+        break
+    
+
+elif valo == 2:
+
+    propina = total * 0.12
+
+    print("\n/////////////////////////////////////////////////////////")
+    print("\n El monto asciende a:", propina + total)
+    print("\n La propina a pagar es de:", propina)
+   
+    while div == True:
+    
+        print("\n Monto por persona:", total / pers )
+        break
+
+elif valo == 3:
+
+    propina = total * 0.10
+
+    print("\n/////////////////////////////////////////////////////////")
+    print("\n El monto asciende a:", propina + total)
+    print("\n La propina a pagar es de:", propina)
+    
+    while div == True:
+    
+        print("\n Monto por persona:", total / pers )
+        break
+else:
+
+    print("\n No es una opcion correcta \n Intente de nuevo")
+
